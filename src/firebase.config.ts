5bc1128c8a6f701b0e20ec84a3e5fbdccf782842
James Edmonds <james@deckchairlabs.com>
2020-02-24 09:34:24 +1030

    simplified firebase config

---
+++ src/firebase.config.ts
@@ -0,0 +1,12 @@
+const projectId = process.env.FIREBASE_PROJECT_ID
+
+export default {
+  projectId,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  storageBucket: `${projectId}.appspot.com`,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
